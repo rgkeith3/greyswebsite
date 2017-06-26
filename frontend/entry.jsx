43d4915c4ae224e4635699e23348c348157f5db0
Grey Keith <r.grey.keith@gmail.com>
2017-06-26 11:34:35 -0700

    Initial Commit

---
+++ frontend/entry.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const ()
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<Root/>, document.getElementById('main'));
+});
